9d18434ded8d12dff3562cc7b2a910fcfb17e2da
Paras Sharma <sharmaparas23@gmail.com>
2026-09-12 18:06:21 +0530

    Data Validation Completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -19,6 +19,8 @@ TRAIN_FILE_NAME: str = "train.csv"
 
 TEST_FILE_NAME: str = "test.csv"
 
+SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
+
 
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
@@ -35,5 +37,17 @@ DATA_INGESTION_FEATURE_STORE_DIR_NAME: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR_NAME: str = "ingested"
 
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+
+"""
+Data Validation related constant start with DATA_VALIDATION VAR NAME
+
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR_NAME: str = "validated"
+DATA_VALIDATION_INVALID_DIR_NAME: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR_NAME: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 
 
--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -13,4 +13,24 @@ class DataIngestionArtifact:
         metadata (dict): Additional metadata associated with the artifact.
     """
     trained_file_path: str
-    test_file_path: str 
+    test_file_path: str
+
+
+@dataclass
+class DataValidationArtifact:
+    """
+    Represents an artifact related to data validation in network security.
+
+    Attributes:
+        name (str): The name of the artifact.
+        type (str): The type of the artifact (e.g., 'validation_report', 'validation_summary').
+        source (str): The source from which the artifact was generated.
+        timestamp (str): The timestamp when the artifact was created or validated.
+        metadata (dict): Additional metadata associated with the artifact.
+    """
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str   
